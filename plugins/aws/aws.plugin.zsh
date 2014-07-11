1da95850974755ac06c61ed600dae45924ffb57c
Cristian Măgherușan-Stanciu <cristian.magherusan-stanciu@here.com>
2014-07-11 17:55:06 +0200

    aws: added an alias for changing the AWS access key set on a profile
    style cleanups
    more obvious error messages
    switch authentication against .aws/credentials

--- plugins/aws/aws.plugin.zsh
+++ plugins/aws/aws.plugin.zsh
@@ -1,26 +1,26 @@
-_homebrew-installed() {
+export AWS_HOME=~/.aws
+
+function _homebrew-installed () {
   type brew &> /dev/null
 }
 
-_awscli-homebrew-installed() {
-  brew list awscli &> /dev/null
+function _awscli-homebrew-installed () {
+  brew --prefix awscli &> /dev/null
 }
 
-export AWS_HOME=~/.aws
-
-function agp {
+function agp () {
   echo $AWS_DEFAULT_PROFILE
 }
-function asp {
+
+function asp () {
   export AWS_DEFAULT_PROFILE=$1
   export AWS_PROFILE=$1
   export RPROMPT="<aws:$AWS_DEFAULT_PROFILE>$RPROMPT"
 }
-function aws_profiles {
-  reply=($(grep profile $AWS_HOME/config|sed -e 's/.*profile \([a-zA-Z0-9_-]*\).*/\1/'))
-}
 
-compctl -K aws_profiles asp
+function aws_profiles () {
+  reply=($(grep '^\[' $AWS_HOME/credentials|sed -e 's/\[\(.*\)\]/\1/'))
+}
 
 if _homebrew-installed && _awscli-homebrew-installed ; then
   _aws_zsh_completer_path=$(brew --prefix awscli)/libexec/bin/aws_zsh_completer.sh
@@ -30,3 +30,32 @@ fi
 
 [ -x $_aws_zsh_completer_path ] && source $_aws_zsh_completer_path
 unset _aws_zsh_completer_path
+
+function chpwd () {
+  emulate -L zsh
+  test -f .aws_profile && asp $(cat .aws_profile)
+}
+
+
+function aws_change_access_key () {
+  if [[ "x$1" == "x" ]] then
+    echo "usage: $0 <profile.name>"
+    return 1
+  else
+    echo "Insert the credentials when asked."
+    asp $1
+    aws iam create-access-key
+    aws configure --profile $1
+    echo "You can now safely delete the old access key running 'aws iam delete-access-key --access-key-id ID'"
+    echo "Your current keys are:"
+    aws iam list-access-keys
+  fi
+}
+
+compctl -K aws_profiles asp aws_change_access_key
+
+if _homebrew-installed && _awscli-homebrew-installed; then
+  source $(brew --prefix)/opt/awscli/libexec/bin/aws_zsh_completer.sh
+else
+  source `which aws_zsh_completer.sh`
+fi
